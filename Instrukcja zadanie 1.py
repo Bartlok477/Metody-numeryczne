368915cad7ebf407dfe5cfe1ab3d3ae3225266f0
Bartłomiej Książek <bartlok477@gmail.com>
2019-10-30 14:16:50 +0100

    1 zadanie

---
+++ Instrukcja zadanie 1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 12:19:28 2019
+
+@author: ZUISA
+"""
+
+from scipy import *
+from pylab import *
+x=r_[0:101]
+y01=sin(2*pi*x/100)
+y02=cos(2*pi*x/100)
+plot(x,y01,linewidth=5.0)
+#hold(True)
+plot(x,y02,linewidth=5.0)
+xlabel('x');
+ylabel('y');
+title('Plottingsin(x) & cos(x)');
+legend(('sin(x','cos(x)'));
+grid(True)
+
---
+++ Wyznaczniki maciewrzy.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 12:14:49 2019
+
+@author: ZUISA
+"""
+
+from matplotlib import *
+import numpy as np 
+
+
+x=np.array([[ 2, 3],[1,1]])
+wyznacznik1=np.linalg.det(x)
+
+print(wyznacznik1)
+
+
+y=np.array([[ 2, 3,2],[1,4,3],[1,2,6]])
+
+wyznacznik2=np.linalg.det(y)
+
+print(wyznacznik2)
